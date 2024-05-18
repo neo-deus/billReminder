2aab63e8624edac66f84b7c641c225042f3a5c8c
Arpan De <arpandesp7890@gmail.com>
2024-05-18 10:28:05 +0530

    mini

--- api/reminder.js
+++ api/reminder.js
@@ -23,31 +23,32 @@ const reminderSchema = new mongoose.Schema({
   module.exports = async (req, res) => {
     const now = new Date(new Date().toISOString());
     try {
-      const reminders = await Reminder.find({ reminderDateTime: { $lte: now } });
-      if (reminders.length > 0) {
-        // Map each reminder to a promise of sending an email and deleting the reminder
-        const operations = reminders.map(reminder => 
-          (async () => {
-            const mailOptions = {
-              from: "ritushreedas20027@gmail.com",
-              to: reminder.email,
-              subject: 'FinanceFolio',
-              text: `Hi there! Just a reminder that your ${reminder.billName} bill of amount ${reminder.amount} is due as on ${reminder.reminderDateTime}.`,
-            };
-            await transporter.sendMail(mailOptions);
-            console.log(`Email sent to ${reminder.email}`);
-            await Reminder.findByIdAndDelete(reminder._id);
-            console.log(`Reminder with ID ${reminder._id} deleted from the database.`);
-          })()
-        );
+      // const reminders = await Reminder.find({ reminderDateTime: { $lte: now } });
+      // if (reminders.length > 0) {
+      //   // Map each reminder to a promise of sending an email and deleting the reminder
+      //   const operations = reminders.map(reminder => 
+      //     (async () => {
+      //       const mailOptions = {
+      //         from: "ritushreedas20027@gmail.com",
+      //         to: reminder.email,
+      //         subject: 'FinanceFolio',
+      //         text: `Hi there! Just a reminder that your ${reminder.billName} bill of amount ${reminder.amount} is due as on ${reminder.reminderDateTime}.`,
+      //       };
+      //       await transporter.sendMail(mailOptions);
+      //       console.log(`Email sent to ${reminder.email}`);
+      //       await Reminder.findByIdAndDelete(reminder._id);
+      //       console.log(`Reminder with ID ${reminder._id} deleted from the database.`);
+      //     })()
+      //   );
         
-        // Wait for all operations to complete
-        await Promise.all(operations);
-        return res.status(200).json({ msg: 'Reminders processed successfully' });
-      } else {
-        console.log('No reminders match the current date and time.');
-        return res.status(200).json({ msg: 'No reminders to process' });
-      }
+      //   // Wait for all operations to complete
+      //   await Promise.all(operations);
+      //   return res.status(200).json({ msg: 'Reminders processed successfully' });
+      // } else {
+      //   console.log('No reminders match the current date and time.');
+      //   return res.status(200).json({ msg: 'No reminders to process' });
+      // }
+      return res.status(200).json({ msg: "hi hello" });
     } catch (error) {
       console.error('Error processing reminders:', error);
       return res.status(500).json({ error: 'Failed to process reminders' });
